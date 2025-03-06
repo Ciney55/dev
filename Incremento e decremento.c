0521ea9e77b20a64845d30a9889d769a878e8931
Ciney55 <walcineysilva3@gmail.com>
2025-03-05 21:25:16 -0300

    exemplos de conversão.

---
+++ Incremento e decremento.c
@@ -0,0 +1,18 @@
+Incremento e decremento.c
+#include<stdio.h>
+
+int main(){
+/*
+incremento (++)
+pré-incremento ++a
+pós-incremento a++
+decremento (--)
+pré-decremento --a
+pós-decremento a--
+*/
+
+int número1 = 1;
+
+printf("antes incremento: %d\n", número1);
+número1 ++;
+printf("apos incremento: %d\n", numero1);
